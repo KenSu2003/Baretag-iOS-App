8ead3008edf49faa268ea6b0619f8ead874053a1
Ken Su <kensu324@gmail.com>
2025-01-21 22:09:42 -0500

    Initial Commit

---
+++ Baretag iOS App/Baretag_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baretag_iOS_AppApp.swift
+//  Baretag iOS App
+//
+//  Created by Ken Su on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Baretag_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baretag iOS AppTests/Baretag_iOS_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Baretag_iOS_AppTests.swift
+//  Baretag iOS AppTests
+//
+//  Created by Ken Su on 1/21/25.
+//
+
+import Testing
+@testable import Baretag_iOS_App
+
+struct Baretag_iOS_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
